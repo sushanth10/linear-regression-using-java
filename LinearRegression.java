d4c4c165b80bd086dbadacb044276baeceb2c547
Sushanth Bhat <sushanthbhat15@gmail.com>
2022-04-05 23:58:12 +0530

    model ready

--- LinearRegression.java
+++ LinearRegression.java
@@ -1,16 +1,15 @@
 import java.util.*;
-import java.math.*;
 
 public class LinearRegression{
-    ArrayList<Long[]> al = new ArrayList<Long[]>();
-    static long avg_x=0, avg_y=0;
+    ArrayList<double[]> al = new ArrayList<double[]>();
+    static double avg_x=0, avg_y=0;
     double sd_x=0, sd_y=0;
     static long n = 0;
-    static long sum_x=0, sum_y=0, sum_XY=0;
+    static double sum_x=0, sum_y=0, sum_XY=0;
     double coef_ = 0, slope_ =0;
     double intercept_ = 0;
 
-    void addCoordinate(Long[] coord){
+    void addCoordinate(double[] coord){
         this.al.add(coord);
         n++;
         sum_x+= coord[0];
@@ -20,14 +19,16 @@ public class LinearRegression{
     }
 
     void calcStandardDeviation(){
-        long sum_X_sq=0, sum_Y_sq=0;
-        for(Long[] coord : al){
+        double sum_X_sq=0, sum_Y_sq=0;
+        for(double[] coord : al){
             sum_X_sq += (coord[0]-avg_x)*(coord[0]-avg_x);
             sum_Y_sq += (coord[1]-avg_y)*(coord[1]-avg_y);
             sum_XY += (coord[0]-avg_x)*(coord[1]-avg_y);
         }
-        this.sd_x = Math.pow(sum_X_sq, 0.5);
-        this.sd_y = Math.pow(sum_Y_sq, 0.5);
+        this.sd_x = Math.pow(sum_X_sq/n, 0.5);
+        this.sd_y = Math.pow(sum_Y_sq/n, 0.5);
+
+        System.out.println("Sum squares : "+sum_X_sq+" "+sum_Y_sq);
 
     }
 
@@ -38,24 +39,53 @@ public class LinearRegression{
     }
 
     void displayResults(){
-        
+
+        // use following lines for checking other values
+        // System.out.println("Coordinates : "+al);
+        // System.out.println("Means x and y : "+avg_x+" "+avg_y);
+        // System.out.println("Std x and y : "+sd_x+" "+sd_y);
+        // System.out.println("Sums : "+sum_XY+" "+sum_x+" "+sum_y);
+
+        if(this.coef_>0.5 || this.coef_<-0.5){
+            System.out.println("Well co-related data. Worth creating the linear model");
+        }else{
+            System.out.println("Not very co-related data.");
+        }
+        System.out.println("Coefficient of correlation : "+this.coef_);
+        System.out.println("Linear Model created with equation : y="+this.slope_+"x+"+this.intercept_);
+    }
+
+    void fit(){
+        this.calcStandardDeviation();
+        this.calcCorrelation();
+        this.displayResults();
+    }
+
+    double predict(double X){
+        double Y = this.slope_*X+this.intercept_;
+        return Y;
     }
 
     public static void main(String args[]){
         System.out.println("-----PROGRAM STARTED------\n");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of coordinates : ");
-        long n = sc.nextLong();
+        System.out.print("Enter number of coordinates : ");
+        long N = sc.nextLong();
         LinearRegression lm = new LinearRegression();
 
-        for(long i=0; i<n; i++){
-            System.out.println("Enter coordinate : ");
-            Long a = sc.nextLong();
-            Long b = sc.nextLong();
-            Long[] coord = {a,b};
+        for(long i=0; i<N; i++){
+            System.out.print("Enter coordinate : ");
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double[] coord = {a,b};
             lm.addCoordinate(coord);
         }
+        lm.fit();
 
+        System.out.print("Enter value of X for prediction : ");
+        double X = sc.nextDouble();
+        double Y = lm.predict(X);
+        System.out.println("Predicted Y value : "+Y);
         
         sc.close();
     }
